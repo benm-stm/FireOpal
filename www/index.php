2b5d0c9d0435051bed7f8418c1ddf08fee394e11
ounish <hatem.ounis@st.com>
2011-08-11 15:13:29 +0100

    Fix includes.

--- www/index.php
+++ www/index.php
@@ -321,8 +321,7 @@ function prepare_files($filesArray, $prefix) {
                         flush();
                         if (isset($_REQUEST['tests_to_run'])) {
                             // manage request
-                            require_once '../include/IntegrationTests.class.php';
-                            require_once '../include/CustomResultPrinter.class.php';
+                            require_once 'IntegrationTests.class.php';
                             $suite = new IntegrationTests();
                             $files = prepare_files($_REQUEST['tests_to_run'], '../tests');
                             $suite->addFiles($files);
