2aef0568fb3706e60770b15c4ca3213fc2bad735
Hatem Ounis <hatem.ounis@st.com>
2012-02-06 15:22:44 +0100

    Way much simpler test

--- tests/test.rb
+++ tests/test.rb
@@ -1,32 +1,10 @@
-require "rubygems"
-require "selenium-webdriver"
-require "test/unit"
-require 'capybara'
-require 'capybara/dsl'
+require 'rubygems'
+require 'selenium-webdriver'
 
-class RubyUnitWebdriverAddartifact < Test::Unit::TestCase
-include Capybara::DSL
-  def setup
-    Capybara.current_driver = :selenium
-    Capybara.app_host = 'http://codex.cro.st.com'
-    Capybara.run_server = false
-    Capybara.register_driver :selenium do |app|
-      Capybara::Selenium::Driver.new(app, {:browser => :remote, :url => "http://10.157.15.160:4444/wd/hub"})
-    end
-    visit('/')
-  end
-  
-  def login
-    find(:xpath, "//a[@href='/my/']").click
-    fill_in('form_loginname', :with => 'blah')
-    fill_in('form_pw', :with => 'bloh')
-    find("input[type='submit']").click
-    page.has_content?('Personal Page')
-  end
-  
-  def test_
-    self.login
-  end
-  
-    #page.driver.browser.save_screenshot("/tmp/test.png")
+begin
+  driver = Selenium::WebDriver.for :remote, :url => "http://server:4444/wd/hub", :desired_capabilities => :firefox
+  driver.get "http://codex"
+  driver.save_screenshot "/tmp/test.png"
+ensure
+  driver.quit
 end
