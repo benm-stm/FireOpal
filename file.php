18ac2a9b0c2bda6712231222ff94c10d6cb8b65b
Hatem Ounis <hatem.ounis@st.com>
2012-02-10 17:16:03 +0100

    retweak regexes

--- file.php
+++ file.php
@@ -1,45 +1,49 @@
 <?php
-    $filename = '/prj/codex/terzino/tuleap.rb';
-	$className = basename($filename, '.rb');
-    $file = file_get_contents($filename);
+
+$filename  = '/home/ounis/AutomaticTests/tests/tuleap.rb';
+$className = basename($filename, '.rb');
+$file      = file_get_contents($filename);
+
+#### Include & require
+$fileContent = "require \"rubygems\"\n";
+$fileContent .= "gem \"rspec\"\n";
+$fileContent .= "require \"rspec/autorun\"\n";
+$fileContent .= "require '".$className."'\n";
+
+#### Class definition turned on describe
+#### setup, teardown & login call
+$pattern = '/class (\w+)/i';
+$replacement = 'describe ${1} do 	
+    before(:each) do
+        @bowling = ${1}.new 
+        @bowling.setup() 
+        @bowling.login()
+    end
+
+    after(:each) do
+        @bowling.teardown()
+    end';
+
+$fileContent .= preg_replace($pattern, $replacement, $file);
+
+$pattern = '/def setup(.*)\send/isUe';
+$fileContent = preg_replace($pattern, '', $fileContent);
+
+#### teardown
+$pattern     = '/def teardown(.*)\send/isUe';
+$fileContent = preg_replace($pattern, '', $fileContent);
+
+#### login
+$pattern     = '/def login(.*)\send/isUe';
+$fileContent = preg_replace($pattern, '', $fileContent);
 
 
-    #### Include & require
-	$fileContent = "require \"rubygems\"\n";
-    $fileContent .= "gem \"rspec\"\n";
-    $fileContent .= "require \"rspec/autorun\"\n";
-    $fileContent .= "require '".$className."'\n";
+$pattern     ='/def (\w+)\s(.*)\send/isUe';
 
-	#### Class definition turned on describe
-	#### setup, teardown & login call
-	$pattern = '/class (\w+)/i';
-	$replacement = 'describe ${1} do 	
-	            before(:each) do
-	            @bowling = ${1}.new 
-	            @bowling.setup() 
-                @bowling.login()
-	        end
+$fileContent = preg_replace($pattern, '"$1"', $fileContent);
 
-	after(:each) do
-	@bowling.teardown()
-	end';
-	
-	$fileContent .= preg_replace($pattern, $replacement, $file);
-    
-	$pattern ='/def setup([^"]*)end/isUe';
-    $fileContent = preg_replace($pattern, '', $fileContent);
-    
-	#### teardown
-	$pattern ='/def teardown([^"]*)end/isUe';
-    $fileContent = preg_replace($pattern, '', $fileContent);
-	
-	#### login
-	$pattern ='/def login([^"]*)end/isUe';
-    $fileContent = preg_replace($pattern, '', $fileContent);
+$fileContent = preg_replace('/\s+\n/', "\n", $fileContent);
 
-	
-    $pattern ='/def (\w+)\s[^"]*end/isUe';
-	
-	$fileContent = preg_replace($pattern, '$1', $fileContent);
-    file_put_contents($className.'_spec.rb', $fileContent);
+file_put_contents($className.'_spec.rb', $fileContent);
+
 ?>
