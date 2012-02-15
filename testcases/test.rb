22b69b0734b7a934f1b1e86153072d67c1c36364
Hatem Ounis <hatem.ounis@st.com>
2012-02-15 17:50:30 +0100

    Make testcases examples respect new format

--- testcases/test.rb
+++ testcases/test.rb
@@ -1,26 +1,14 @@
-require 'rubygems'
-require 'selenium-webdriver'
-require 'rspec/autorun'
-
-class Test
-    def run()
-        driver = Selenium::WebDriver.for :remote, :url => "http://server:4444/wd/hub", :desired_capabilities => :firefox
-        driver.get "http://codex"
-        describe "Test logging in" do
-            driver.find_element(:name, "form_loginname").send_keys "login"
-            driver.find_element(:name, "form_pw").send_keys "password"
-            driver.find_element(:name, "login").click
-            it "Test the wrong title of the page" do 
-                (driver.title).should == "wrong title"
-            end
-            it "Test the correct title of the page" do 
-                (driver.title).should == "good tiltle"
-            end
-            it "compare 1 to 1" do
-                1.should eq(1)
-            end
-            driver.save_screenshot "/tmp/test.png"
-            driver.quit
-        end
+describe "Test logging in" do
+    @driver.find_element(:name, "form_loginname").send_keys "login"
+    @driver.find_element(:name, "form_pw").send_keys "password"
+    @driver.find_element(:name, "login").click
+    it "Test the wrong title of the page" do 
+        (@driver.title).should == "wrong title"
+    end
+    it "Test the correct title of the page" do 
+        (@driver.title).should == "good tiltle"
+    end
+    it "compare 1 to 1" do
+        1.should eq(1)
     end
 end
