3255533d9a27b68f428221f5d1685321f3c0fb36
Ahmed HOSNI <hosniah@tunlx007.tun.st.com>
2012-02-22 11:07:05 +0100

    Add dummy JS to store testCases in a HTML input

--- www/cases.php
+++ www/cases.php
@@ -137,16 +137,11 @@ if (!empty($testsuites)) {
                                         <td><input type="radio" name="load_testsuites" value="'.$t.'" /></td>
                                     </tr>';
     }
-    //to be modified
-    $arr = implode(',' , array("test1.rb", "test2.rb"));
-
     echo '
                                 </table>
                             </fieldset>
                             <div id="submit_panel">
-                                <input type="hidden" name="testcases_loaded" id="testcases_loaded" value="'.$arr.'">
-                                <input type="button" name="load" value="Load" onClick="loadTestCases( this.form, this.form.testcases_to_add)">
-                            
+                                <input type="button" name="load" value="Load" onclick="loadTestCases(this.form,this.form.testSuite,this.form.testCases);">
                             </div>';
 }
 
@@ -161,24 +156,40 @@ echo '
                                         </td>
 
                                         <td style="vertical-align:middle">
-                                            <input type="button" value="Add >>>" onClick="AddtestCases(this.form.testCases,this.form.testcases_to_add)">
+                                            <input type="button" value="Add >>>" onClick="AddtestCases(this.form.testCases,this.form.testSuite)">
                                         </td>
 
                                         <td align="center"><FONT size="2"><B>Dispatched test cases</B></FONT>
-                                            <select align=top name="testcases_to_add" size=10  style="width:320px" multiple="multiple">
+                                            <select align=top name="testcases_to_add" size=6 style="width:220px">
                                                 <option value="10">----------------------</option>
                                             </select>
                                         </td>
                                         <td style="vertical-align:middle">
-                                            <input type="button" value="Remove >>>" onClick="RemoveTestCase(this.form.testcases_to_add,this.form.testCases)">
+                                            <input type="button" value="Remove >>>" onClick="RemoveTestCase(this.form.testSuite,this.form.testCases)">
                                         </td>
                                     </tr>
                                 </table>
 
                                 <SCRIPT language="javascript">
-                                    document.EditTestSuiteForm.testcases_to_add.options.length=0;
+                                    document.EditTestSuiteForm.testSuite.options.length=0;
                                 </SCRIPT>
-
+                                <script type="text/javascript">
+                                    function generateTestSuite(testcases_to_add) {
+                                        var p = document.getElementById(\'testcases_to_add\');
+                                        var testCasesString = "";
+                                            for(testCase=0;testCase<p.length;testCase++) {
+                                                if(testCase+1 == p.length) {
+                                                    testCasesString += p[testCase].value;
+                                                } else {
+                                                    testCasesString += p[testCase].value+",";
+                                                }
+                                            }
+                                        //alert(testCasesString);
+                                        d = document.getElementById("submit_panel_1");';
+echo "                                  d.innerHTML = '<input type=\"text\" id=\"testcases_to_add\" name=\"testcases_to_add\" value=\"' + testCasesString + '\" />';
+                                    }
+                            </script>";
+echo '
                                 <table nowrap>
                                     <tr>
                                         <td>Name:</td>
@@ -190,6 +201,7 @@ echo '
                                     </tr>
                                 </table>
                             </fieldset>
+                            <div id="submit_panel_1"> </div>
                             <div id="submit_panel">
                                 <input id="generate" type="submit" value="Generate !"/>
                             </div>
