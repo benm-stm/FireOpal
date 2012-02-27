a2306c7d0f153d32141d701c12326392cc06be63
Hatem Ounis <hatem.ounis@st.com>
2012-02-27 12:36:02 +0100

    Introduce a testcase template.

---
+++ testcases/template.rb
@@ -0,0 +1,17 @@
+# docman/test.rb
+# trackerV3/tuleap.rb
+
+describe "Testcase name" do
+    it "Fill a form and click on a button" do
+        @driver.find_element(:name, "form_element_example_1").clear
+        @driver.find_element(:name, "form_element_example_1").send_keys @setup['user']['value']
+        @driver.find_element(:name, "form_element_example_2").send_keys @setup['password']['value']
+        @driver.find_element(:name, "button_example").click
+    end
+    it "Test the wrong title of the page" do 
+        (@driver.title).should == "wrong title"
+    end
+    it "Test the correct title of the page" do 
+        (@driver.title).should == "good tiltle"
+    end
+end
