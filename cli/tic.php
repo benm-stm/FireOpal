bca1243d3046b9c1753229a4ee9043c52f51f7fd
Hatem Ounis <hatem.ounis@st.com>
2012-02-27 10:02:18 +0100

    Prepare the display of help.

--- cli/tic.php
+++ cli/tic.php
@@ -20,15 +20,19 @@ require_once '../include/Setup.class.php';
 require_once '../include/TestSuite.class.php';
 require_once '../include/TestSuiteManager.class.php';
 
-for ($i = 1; $i <= $argc-1; $i++) {
+$displayHelp = false;
+$verbose     = false;
+$function    = '';
+$parameters  = array();
+
+for ($i = 1; $i < $argc; $i++) {
     if ($argv[$i] == "--help" || $argv[$i] == "-h") {
-        $display_help = true;
+        $displayHelp = true;
     } elseif ($argv[$i] == "--verbose" || $argv[$i] == "-v") {
         $verbose = true;
     } elseif (!preg_match("/^-/", $argv[$i])) {
         // Not a parameter (does not start with "-") Then, it must be a name of a function
         $function = $argv[$i];
-        break;
     } elseif (preg_match("/^-/", $argv[$i])) {
         // A parameter for the given function
         $parameters[] = $argv[$i];
@@ -42,35 +46,56 @@ for ($i = 1; $i <= $argc-1; $i++) {
 if (!empty($function)) {
     switch ($function) {
         case 'setup' :
-            $setup = new Setup();
-            $set   = $setup->load();
-            foreach ($set as $name => $entry) {
-                if ($entry['type'] == 'password') {
-                    echo " *".$name.": ******** (".$entry['description'].")\n";
-                } else {
-                    echo " *".$name.": ".$entry['value']." (".$entry['description'].")\n";
+            if ($displayHelp) {
+                echo "Display setup.\n";
+            } else {
+                $setup = new Setup();
+                $set   = $setup->load();
+                foreach ($set as $name => $entry) {
+                    if ($entry['type'] == 'password') {
+                        echo " * ".$name.": ******** (".$entry['description'].")\n";
+                    } else {
+                        echo " * ".$name.": ".$entry['value']." (".$entry['description'].")\n";
+                    }
                 }
             }
             break;
         case 'testsuites' :
-            $testSuiteManager = new TestSuiteManager();
-            $testsuites       = $testSuiteManager->searchTestsuites();
-            foreach ($testsuites as $testsuite => $testcases) {
-                echo " *".$testsuite."\n";
+            if ($displayHelp) {
+                echo "Display testsuites.\n";
+            } else {
+                $testSuiteManager = new TestSuiteManager();
+                $testsuites       = $testSuiteManager->searchTestsuites();
+                foreach ($testsuites as $testsuite => $testcases) {
+                    echo " * ".$testsuite.": \"".$testcases."\"\n";
+                }
             }
             break;
         case 'testcases' :
-            echo "Not implemented yet\n";
+            if ($displayHelp) {
+                echo "Display testcases.\n";
+            } else {
+                echo "Not implemented yet\n";
+            }
             break;
         case 'generate' :
-            echo "Not implemented yet\n";
+            if ($displayHelp) {
+                echo "Generate a testsuite.\n";
+            } else {
+                echo "Not implemented yet\n";
+            }
             break;
         default :
             echo "Unknown function\n";
             break;
     }
 } else {
-    echo "TIC \"TIC is not CLI\"\nFunctions:\n *setup: Display setup.\n *testsuites: Display testsuites.\n *testcases: Display testcases.\n *generate: Generate a testsuite.\n";
+    echo "TIC \"TIC is not CLI\"\nFunctions:
+    * setup      : Display setup.
+    * testsuites : Display testsuites.
+    * testcases  : Display testcases.
+    * generate   : Generate a testsuite.
+    \n";
 }
 
 ?>
