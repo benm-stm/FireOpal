7529da615005b1b82c166dc53353129fce45eda4
Hatem Ounis <hatem.ounis@st.com>
2012-03-05 17:32:51 +0100

    Prepare the form of a real life test.

---
+++ testcases/docman/CopyPasteMenu.rb
@@ -0,0 +1,22 @@
+#--- Start tags
+# read_only
+# docman_write
+#--- End tags
+
+describe "Display of paste in menu" do
+    it "Search a folder other than docman root" do
+        
+    end
+    it "Copy the selected folder" do
+        
+    end
+    it "Paste doesn't exist in the menu of the copied folder" do
+        
+    end
+    it "Paste doesn't exist in the menu of a children folder of the copied folder" do
+        
+    end
+    it "Paste exist in the menu of a parent of the copied folder" do
+        
+    end
+end
--- testcases/template.rb
+++ testcases/template.rb
@@ -4,8 +4,9 @@
 #--- End tags
 
 #--- Start dependency list
-# docman/test.rb
+# docman/CopyPasteMenu.rb
 # trackerV3/tuleap.rb
+# template.rb
 #--- End dependency list
 
 describe "Testcase name" do
@@ -15,10 +16,10 @@ describe "Testcase name" do
         @driver.find_element(:name, "form_element_example_2").send_keys @setup['password']['value']
         @driver.find_element(:name, "button_example").click
     end
-    it "Test the wrong title of the page" do 
+    it "Test the wrong title of the page" do
         (@driver.title).should == "wrong title"
     end
-    it "Test the correct title of the page" do 
+    it "Test the correct title of the page" do
         (@driver.title).should == "good tiltle"
     end
 end
