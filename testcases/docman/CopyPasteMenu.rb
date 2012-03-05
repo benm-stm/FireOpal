688c5cd8e531a80342eac30165ecbd65d500a9ed
Hatem Ounis <hatem.ounis@st.com>
2012-03-05 17:56:32 +0100

    Better form for the test.

--- testcases/docman/CopyPasteMenu.rb
+++ testcases/docman/CopyPasteMenu.rb
@@ -4,13 +4,25 @@
 #--- End tags
 
 describe "Display of paste in menu" do
-    it "Search a folder other than docman root" do
+    it "User is document writer on docman root" do
+        
+    end
+    it "Find a folder other than docman root" do
+        
+    end
+    it "User is document writer on the selected folder" do
         
     end
     it "Copy the selected folder" do
         
     end
     it "Paste doesn't exist in the menu of the copied folder" do
+        
+    end
+    it "Find a children folder of the copied one" do
+        
+    end
+    it "User is document writer on the selected folder" do
         
     end
     it "Paste doesn't exist in the menu of a children folder of the copied folder" do
