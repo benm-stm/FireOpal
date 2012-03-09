bdeac9485c03bf0a20d19f59127e907551a5da3b
Ahmed HOSNI <hosniah@tunlx007.tun.st.com>
2012-03-09 16:03:55 +0100

    Make test Compliant with team  testing spirit

--- testcases/AddWidget.rb
+++ testcases/AddWidget.rb
@@ -1,29 +1,27 @@
 #--- Start tags
 # write
 #--- End tags
-describe "add a widget to My dashboard" do
+describe "Add a widget to My dashboard" do
     describe "#precondition:" do
-        it "should find my personal page" do
+        it "Find my personal page" do
             @driver.find_element(:link, "My Personal Page").click
         end
-        it "should find customize" do
+        it "Find customize link" do
             @driver.find_element(:link, "Customize").click
         end
-        it "should find widget category" do
-            @driver.find_element(:link, @setup['widget_category']['value']).click
+        it "Find widget category" do
+            @driver.find_element(:link, "Source Code Management").click
         end
-    end
-    describe "add the widget" do
-        it "should hint the add button of the widget" do
-            @driver.find_element(:name, "name["+@setup['widget_name']['value']+"][add]").click
+        it "Hint the add button of the widget" do
+            @driver.find_element(:name, "name[mylatestsvncommits][add]").click
             @driver.manage.timeouts.implicit_wait = 5
         end
     end
     describe "#regression:" do
-        it "should find my personal page" do
+        it "Find my personal page" do
             @driver.find_element(:link, "My Personal Page").click
         end
-        it "look for the widget on My dashboard" do
+        it "Look for the widget on My dashboard" do
             #@TODO find a way to check that the widget was added, you have probably to review widgets css/Html structure       
             @driver.manage.timeouts.implicit_wait = 5
         end
