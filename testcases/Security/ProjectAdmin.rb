7f6f392036c5f4d4b8b194c824357da73b248556
GHOFRANE SOUAKI <souakig@tunlx107.(none)>
2012-07-30 10:30:10 +0100

    Security tests

---
+++ testcases/Security/ProjectAdmin.rb
@@ -0,0 +1,31 @@
+#--------Test Summary------------
+#this test Check that a non-project admin can NOT access to the admin page.
+#
+#--------Config dependencies-----
+# host
+# project_id
+# project_short_name 
+#--------Test(s) dependencies----
+#            
+
+
+describe "non-project admin" do
+	describe "#---------precondition------------:" do
+		it "log to the project link" do
+			$link = @setup['host']['value'] + '/projects/' + @setup['project_short_name']['value']
+		@driver.navigate.to $link
+		end
+		it "test if the user is a project member" do
+			(@driver.find_element(:class, "contenttable").text.include? "Permission Denied").should be_false
+		end
+	end
+	describe "#--------steps-----------------:" do
+		it "try to access to project admin section" do
+			$link = @setup['host']['value'] + '/project/admin/?group_id=' + @setup['project_id']['value']
+		@driver.navigate.to $link
+		end
+		it "display an error message: Insufficient Group Access" do
+			(@driver.find_element(:class, "feedback").text.include? "Insufficient Group Access").should be_true
+		end
+	end
+end
