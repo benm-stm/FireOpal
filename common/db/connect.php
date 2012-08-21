778943ac48cf28abfe6c03a0623fd08c5934677d
Ahmed HOSNI <ahmed.hosni@st.com>
2012-08-21 08:31:56 +0100

    Add db connection stuff

---
+++ common/db/connect.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Copyright (c) STMicroelectronics 2012. All rights reserved
+ *
+ * This code is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this code. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+define('DB_SERVER', 'localhost'); 
+define('DB_SERVER_USERNAME','root');
+define('DB_SERVER_PASSWORD', '');
+define('DB_DATABASE', 'automaticvalidation');
+define('USE_PCONNECT', 'true');
+
+function connect() {  
+if(@mysql_connect(DB_SERVER, DB_SERVER_USERNAME, DB_SERVER_PASSWORD) or die(utf8_encode("Failure while connecting to the server")) )
+    if( @mysql_select_db(DB_DATABASE) or die (utf8_encode("Failure while connecting to the database")))
+        return true;
+    else
+        return false;
+}
+connect();
+?>
