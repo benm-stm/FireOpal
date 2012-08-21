6f42be41b13131d9f8893e58e729eb0c72ec933c
Ahmed HOSNI <ahmed.hosni@st.com>
2012-08-21 08:55:18 +0100

    Fix file header

--- common/sign.php
+++ common/sign.php
@@ -1,4 +1,21 @@
 <?php
+/**
+ * Copyright (c) STMicroelectronics 2012. All rights reserved
+ *
+ * This code is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this code. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 header("Content-Type: text/html; charset=iso-8859-1");
 require_once('User.class.php');
 
