8e26da382781cc8dddd92e80a0ddacdf33c569e9
ounis <hatem.ounis@st.com>
2012-08-22 08:18:32 -0400

    Fix require issue.

--- include/common/User.class.php
+++ include/common/User.class.php
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this code. If not, see <http://www.gnu.org/licenses/>.
  */
-require_once('/db/connect.php');
+require_once('db/connect.php');
 
 class  user {
 
