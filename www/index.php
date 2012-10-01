b24bc35fdf2817ded83c591a2af25466ca30ce6f
Hatem Ounis <hatem.ounis@st.com>
2012-10-01 14:32:17 +0000

    Keep-Alive.

--- www/index.php
+++ www/index.php
@@ -29,6 +29,8 @@ header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
 header("Cache-Control: no-store, no-cache, must-revalidate");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
+header("Connection: Keep-Alive");
+header("Keep-Alive: timeout=300");
 
 if (!isset($_SESSION)) {
     session_start();
