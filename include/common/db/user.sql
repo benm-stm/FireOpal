28c4971318016670829dcbd4a05952da3c635537
Ahmed HOSNI <ahmed.hosni@st.com>
2012-10-03 10:10:12 +0100

    Remove useless db columns

--- include/common/db/user.sql
+++ include/common/db/user.sql
@@ -10,19 +10,6 @@ CREATE TABLE IF NOT EXISTS `user` (
   `familyName` varchar(100) DEFAULT NULL,
   `organisation` varchar(100) DEFAULT NULL,
   `login` varchar(100) DEFAULT NULL,
-  `country` int(11) DEFAULT NULL,
-  `city` varchar(100) DEFAULT NULL,
-  `photo` varchar(100) DEFAULT NULL,
-  `typeOrganisation` int(11) DEFAULT NULL,
-  `activityFocus` varchar(100) DEFAULT NULL,
-  `titleQualification` int(11) DEFAULT NULL,
-  `englishLanguage` varchar(10) DEFAULT NULL,
-  `sex` varchar(100) DEFAULT NULL,
-  `born` varchar(100) DEFAULT NULL,
-  `check1` int(11) DEFAULT '0',
-  `check2` int(11) DEFAULT '0',
-  `check3` int(11) DEFAULT '0',
-  `check4` int(11) DEFAULT '0',
   `completeRecording` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
@@ -31,7 +18,7 @@ CREATE TABLE IF NOT EXISTS `user` (
 -- Table `user` content
 --
 
-INSERT INTO `user` (`id`, `email`, `password`, `surname`, `familyName`, `organisation`, `login`, `country`, `city`, `photo`, `typeOrganisation`, `activityFocus`, `titleQualification`, `englishLanguage`, `sex`, `born`, `check1`, `check2`, `check3`, `check4`, `completeRecording`) VALUES
-(1, 'toto@laposte.net', 'aaa', 'xxxx', 'xxxxx', 'xx', 'xxxx', 74, '1', '52830.jpg', 1, '1', 1, 'no', 'female', '1974', 1, 1, 1, 1, 1);
+INSERT INTO `user` (`id`, `email`, `password`, `surname`, `familyName`, `organisation`, `login`, `completeRecording`) VALUES
+(1, 'toto@laposte.net', 'aaa', 'xxxx', 'xxxxx', 'xx', 'xxxx', 1);
 
--- --------------------------------------------------------
+-------------------------------------------------------------------
