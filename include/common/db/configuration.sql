d8dd5248343ce757b0157395b0195a6607e759a8
Ahmed HOSNI <ahmed.hosni@st.com>
2012-10-08 10:46:57 +0100

    Fix conf table structure

--- include/common/db/configuration.sql
+++ include/common/db/configuration.sql
@@ -2,6 +2,7 @@
   `id` int(11) NOT NULL,
   `userId` int(11) NOT NULL,
   `name` varchar(128) NOT NULL,
-  `type` varchar(128) NOT NULL,
+  `type` varchar(128),
+  `value` varchar(256),
   `Description` text
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
