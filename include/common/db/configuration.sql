0551ac721887135d777d09ab3dcda6895116e525
Ahmed HOSNI <ahmed.hosni@st.com>
2012-10-09 18:36:20 +0100

    Add primary key for conf table

--- include/common/db/configuration.sql
+++ include/common/db/configuration.sql
@@ -1,8 +1,9 @@
 ﻿CREATE TABLE IF NOT EXISTS `configuration` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `userId` int(11) NOT NULL,
   `name` varchar(128) NOT NULL,
   `type` varchar(128),
   `value` varchar(256),
-  `Description` text
+  `Description` text,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
