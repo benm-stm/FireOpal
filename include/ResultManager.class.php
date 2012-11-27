e21be9bd87ecc61369c40a11934f7e36923a68a9
Ahmed HOSNI <ahmed.hosni@st.com>
2012-11-27 15:43:27 +0100

    Add new column with result dashboard for testsuite health

--- include/ResultManager.class.php
+++ include/ResultManager.class.php
@@ -88,6 +88,7 @@ class ResultManager {
                 <tr>
                 <td class="resultHeader">Testsuite</td>
                 <td class="resultHeader">Run date</td>
+        <td class="resultHeader">Health</td>
                 <td class="resultHeader">Output</td>
                 <td class="resultHeader">Download output</td>
                 <td class="resultHeader">Delete</td>
@@ -102,6 +103,9 @@ class ResultManager {
         <td id="resultDate">
             '.date("D M j, Y G:i:s T", $row->date).'
         </td>
+    <td >
+        <span><img src="https://raw.github.com/jenkinsci/jenkins/master/war/src/main/webapp/images/48x48/health-80plus.png"></span>
+    </td>
         <td>
             <fieldset class="fieldset">
                 <legend class="toggler" onclick="toggle_visibility(\'result_output_'.$row->id.'\'); if (this.innerHTML == \'+\') { this.innerHTML = \'-\'; } else { this.innerHTML = \'+\'; }">+</legend>
@@ -109,47 +113,47 @@ class ResultManager {
         //@todo put this stuff in a dedicated method
         //@todo clean up, retrieve xsl stuff from dedicated file
 
-        $xslString = '
-            <?xml version="1.0" encoding="UTF-8"?>
-                <html xsl:version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform" xmlns="http://www.w3.org/1999/xhtml">
-                    <body style="width:550px;font-family:Arial;font-size:12pt;background-color:#EEEEEE">
-                        <h2>Summary</h2>
-                        <xsl:variable name="testCount" select="sum(testsuite/@tests)"/>
-                        <xsl:variable name="errorCount" select="sum(testsuite/@errors)"/>
-                        <xsl:variable name="failureCount" select="sum(testsuite/@failures)"/>
-                        <xsl:variable name="timeCount" select="sum(testsuite/@time)"/>
-                        <xsl:variable name="successRate" select="($testCount - $failureCount - $errorCount) div $testCount"/>
-                        <table border="0" cellpadding="5" cellspacing="2" width="75%">
-                            <tr bgcolor="#A6CAF0" valign="top">
-                                <td><strong>Tests</strong></td>
-                                <td><strong>Failures</strong></td>
-                                <td><strong>Errors</strong></td>
-                                <td><strong>Success rate</strong></td>
-                                <td><strong>Time</strong></td>
-                            </tr>
-                            <tr bgcolor="#FFEBCD" valign="top">
-                                <td><xsl:value-of select="$testCount"/></td>
-                                <td><xsl:value-of select="$failureCount"/></td>
-                                <td><xsl:value-of select="$errorCount"/></td>
-                                <td><xsl:value-of select="$successRate"/></td>
-                                <td><xsl:value-of select="$timeCount"/></td>
-                            </tr>
-                        </table>
-                        <xsl:for-each select="testsuite/testcase">
-                            <div style="background-color:teal;color:white;padding:4px">
-                                <span style="font-weight:bold"><xsl:value-of select="name"/></span>
-                                * <xsl:value-of select="failure"/>
-                            </div>
-                            <div style="margin-left:20px;margin-bottom:1em;font-size:10pt">
-                                <xsl:value-of select="failure"/>
-                                <span style="font-style:italic">
-                                    <xsl:value-of select="failure"/>
-                                </span>
-                            </div>
-                        </xsl:for-each>
-                    </body>
-                </html>
-        ';
+$xslString = '<?xml version="1.0" encoding="UTF-8"?>
+<html xsl:version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform" xmlns="http://www.w3.org/1999/xhtml">
+    <body style="width:650px;font-family:Arial;font-size:11pt;background-color:#EEEEEE">
+        <h2>Summary</h2>
+        <xsl:variable name="testCount" select="sum(testsuite/@tests)"/>
+        <xsl:variable name="errorCount" select="sum(testsuite/@errors)"/>
+        <xsl:variable name="failureCount" select="sum(testsuite/@failures)"/>
+        <xsl:variable name="timeCount" select="sum(testsuite/@time)"/>
+        <xsl:variable name="successRate" select="($testCount - $failureCount - $errorCount) div $testCount"/>
+        <table border="0" cellpadding="5" cellspacing="2" width="85%">
+            <tr bgcolor="#629628" valign="top">
+                <td><strong>Tests</strong></td>
+                <td><strong>Failures</strong></td>
+                <td><strong>Errors</strong></td>
+                <td><strong>Success rate</strong></td>
+                <td><strong>Time</strong></td>
+            </tr>
+            <tr bgcolor="#FFEBCD" valign="top">
+                <td><xsl:value-of select="$testCount"/></td>
+                <td><xsl:value-of select="$failureCount"/></td>
+                <td><xsl:value-of select="$errorCount"/></td>
+                <td><xsl:value-of select="$successRate"/></td>
+                <td><xsl:value-of select="$timeCount"/></td>
+            </tr>
+        </table>
+    <br/>
+        <xsl:for-each select="testsuite/testcase">
+            <div style="background-color:teal;color:white;padding:4px">
+                <span style="font-weight:bold"><xsl:value-of select="name"/></span>
+                * <xsl:value-of select="failure"/>
+            </div>
+            <div style="margin-left:20px;margin-bottom:1em;font-size:10pt">
+                <xsl:value-of select="failure"/>
+                <span style="font-style:italic">
+                    <xsl:value-of select="failure"/>
+                </span>
+            </div>
+        </xsl:for-each>
+    </body>
+</html>
+';
         $xmlDoc = simplexml_load_string($row->output);
         $xslDoc = simplexml_load_string($xslString);
         $proc   = new XSLTProcessor;
