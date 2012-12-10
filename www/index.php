f5cc891867701c1db7cd13990c027e02ef2d772a
Ahmed HOSNI <ahmed.hosni@st.com>
2012-12-10 14:01:14 +0100

    just a commit

--- www/index.php
+++ www/index.php
@@ -24,7 +24,7 @@ require_once('TestSuite.class.php');
 require_once('TestSuiteManager.class.php');
 require_once('common/User.class.php');
 
-header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+header("Expires: Mon, 26 Jul 2017 05:00:00 GMT");
 header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
 header("Cache-Control: no-store, no-cache, must-revalidate");
 header("Cache-Control: post-check=0, pre-check=0", false);
